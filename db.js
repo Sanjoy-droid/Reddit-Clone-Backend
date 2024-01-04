3dc38eb0558560dba139e70f18364fc4651a6250
Sanjoy-droid <sanjoy@sanjoydroid.com>
2024-01-04 20:35:46 +0530

    bug fixed in db.js file

--- db.js
+++ db.js
@@ -1,15 +1,33 @@
+// require("dotenv").config();
+// const mongoose = require("mongoose");
+// const mongoURI = process.env.MONGOURI;
+
+// const connectToMongo = async () => {
+//   try {
+//     mongoose.set("strictQuery", false);
+//     mongoose.connect(mongoURI);
+//     console.log("Connected to mongoDB successfully");
+//   } catch (error) {
+//     console.log(error);
+//     process.exit();
+//   }
+// };
+
+// module.exports = connectToMongo;
+
 require("dotenv").config();
+
+// db.js
 const mongoose = require("mongoose");
-const mongoURI = process.env.MONGO_URI;
+const mongoURI = process.env.MONGOURI;
 
 const connectToMongo = async () => {
   try {
-    mongoose.set("strictQuery", false);
-    mongoose.connect(mongoURI);
-    console.log("Connected to mongoDB successfully");
+    await mongoose.connect(mongoURI);
+    console.log("Connected to MongoDB successfully");
   } catch (error) {
-    console.log(error);
-    process.exit();
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1);
   }
 };
 
--- index.js
+++ index.js
@@ -1,7 +1,33 @@
+// require("dotenv").config();
+// const connectToMongo = require("./db");
+// const express = require("express");
+// var cors = require("cors");
+
+// const app = express();
+// app.use(cors());
+
+// connectToMongo();
+
+// const port = process.env.PORT || 5000;
+
+// app.use(express.json());
+
+// // Available Routes
+// app.get("/hello", (req, res) => {
+//   res.send("hello");
+// });
+// // app.use("/api/auth", require("./routes/auth"));
+// // app.use("/api/post", require("./routes/post"));
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
+
+// index.js
 require("dotenv").config();
 const connectToMongo = require("./db");
 const express = require("express");
-var cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 app.use(cors());
@@ -16,9 +42,7 @@ app.use(express.json());
 app.get("/hello", (req, res) => {
   res.send("hello");
 });
-// app.use("/api/auth", require("./routes/auth"));
-// app.use("/api/post", require("./routes/post"));
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
