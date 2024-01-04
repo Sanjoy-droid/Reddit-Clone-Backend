7256803f036b14818a16ce6b4bb83f23d0fa5fb9
Sanjoy-droid <sanjoy@sanjoydroid.com>
2024-01-04 18:28:54 +0530

    reddit-clone backend made

---
+++ index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const connectToMongo = require("./db");
+const express = require("express");
+var cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+connectToMongo();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+// Available Routes
+app.get("/hello", (req, res) => {
+  res.send("hello");
+});
+// app.use("/api/auth", require("./routes/auth"));
+// app.use("/api/post", require("./routes/post"));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ readme.md
@@ -0,0 +1,12 @@
+install and import bcryptjs
+
+"npm i bcryptjs"
+const bcrypt = require("bcryptjs");
+
+setup the hashing passwords to the database
+
+install and import jsonwebtoken
+
+"npm i jsonwebtoken"
+var jwt = require('jsonwebtoken');
+
